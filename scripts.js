60e17e9c54057efad73949d008bff90c7237b154
master-perfect <106263075+master-perfect@users.noreply.github.com>
2022-05-25 16:49:44 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,29 @@
+//confirm("Hello from master perfect");
+//confirm("Hi users");
+
+        //    var a = 33;
+    //        var b = 10;
+        //    var c = "Test";
+  //          var linebreak = "<br />";
+       //     document.write("your second answer is ---?  ");
+	//		result = (a + b);
+//			document.write(result);
+		//	document.write(linebreak);
+      //      document.write("your answer is ---?  ");
+		//	result = (a == b);
+  //   		document.write(result);
+	//		document.write(linebreak);
+function myFunc() {
+         var x = 6;
+         var y = 9;
+         var sum = x + y; // adds x and y
+         var product = x * y; // multiplies y to x
+         document.getElementById("second").innerText = x;
+		 document.write(x + y);
+         document.getElementById("demo").innerHTML = product;
+         document.getElementById("demo").innerHTML = sum;
+      }
+//  cooments
+	  document.write("From master perfect");
+	  document.createTextNode("h");
+	  
